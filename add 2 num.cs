8b28034bf043c470417244a5d6cd8f02ec3264cd
hasaranga99 <106307238+hasaranga99@users.noreply.github.com>
2022-07-21 13:40:57 +0530

    input
    input program for learn

--- add 2 num.cs
+++ add 2 num.cs
@@ -10,15 +10,28 @@ namespace add_2number
     {
         static void Main(string[] args)
         {
-            int num1;
-            int num2;
-            int ans;
+            /* int num1;
+             int num2;
+             int ans;
 
-            num1 = 32;
-            num2 = 18;
-            ans = num1+ num2;
+             num1 = 32;
+             num2 = 18;
+             ans = num1+ num2;
 
-            Console.WriteLine(num1+ "+" +num2 + "=" +ans);
+             Console.WriteLine(num1+ "+" +num2 + "=" +ans);
+             Console.ReadLine(); ($this is first work$) */
+
+            Console.WriteLine("Enter the First number = ");
+            String number1=Console.ReadLine();
+            int num1=Convert.ToInt32(number1);
+
+            Console.WriteLine("Enter the Second number = ");
+            String number2=Console.ReadLine();
+            int num2=Convert.ToInt32(number2);
+
+            int Total = num1 + num2;
+            Console.WriteLine("Total is = " + Total);
+
             Console.ReadLine();
         }
     }
