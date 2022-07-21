b701da82c8ced99090d7325b7dddcf05eda4f048
hasaranga99 <106307238+hasaranga99@users.noreply.github.com>
2022-07-21 13:01:20 +0530

    ADD 2 NUMBERS
    Writing programs using variables and operators in C#  ( add 2 numbers and display answer

---
+++ add 2 num.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_2number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int ans;
+
+            num1 = 32;
+            num2 = 18;
+            ans = num1+ num2;
+
+            Console.WriteLine(ans);
+            Console.ReadLine();
+        }
+    }
+}
